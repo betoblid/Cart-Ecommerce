48bdebbcf6d417cfe13d638ef2b6c196801c2f13
dev-herbert <soyzabeto@gmail.com>
2024-03-31 23:31:14 -0300

    feat: Criação de banco de dados fake com JSON Server e renderização de cards de produto

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,35 +1,69 @@
+import { useEffect, useState } from "react"
 import { BsCartPlus } from "react-icons/bs"
+import { api } from "../../services"
 
 
-
+interface ProductProps {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    cover: string;
+}
 export const Home = () => {
 
-    return(
+    const [listProduct, setListProduct] = useState<ProductProps[]>([])
+
+    useEffect(() => {
+
+        async function GetProduct() {
+
+            await api.get("/products")
+                .then((response) => {
+                    setListProduct(response.data)
+                })
+        }
+        GetProduct()
+    }, [])
+
+    return (
         <main className="w-full max-w-7xl mx-auto px-4">
-        <h1 className="font-bold mt-10 mb-4 text-center text-2xl ">
-            Produtos em alta
-        </h1>
-        {/* lista de produtos */}
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5 ">
-            <section>
-                <img 
-                className="w-full rounded-lg max-h-70 mb-2 "
-                src="https://m.media-amazon.com/images/I/61CVih3UpdL.__AC_SY445_SX342_QL70_ML2_.jpg" 
-                alt="logo de um fone de ouvido" 
-                />
+            <h1 className="font-bold mt-10 mb-4 text-center text-2xl ">
+                Produtos em alta
+            </h1>
+            {/* lista de produtos */}
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5 ">
 
-                <p className="font-medium mt-1 mb-2">
-                    apple AirPode (3a geração )
-                </p>
+                {
+                    listProduct.map((product) => {
+                        return (
+                            <section key={product.id}>
+                                <img
+                                    className="w-full rounded-lg max-h-70 mb-2 "
+                                    src={product.cover}
+                                    alt={product.title}
+                                />
 
-                <div className="flex gap-3 items-center">
-                    <strong className="text-zinc-700/70">R$ 1.200</strong>
-                    <button className="bg-zinc-900 p-1 rounded">
-                        <BsCartPlus size={20} color="#fff"/>
-                    </button>
-                </div>
-            </section>
-        </div>
+                                <p className="font-medium mt-1 mb-2">
+                                    {product.title}
+                                </p>
+
+                                <div className="flex gap-3 items-center">
+                                    <strong className="text-zinc-700/70">
+                                        {
+                                        product.price.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})
+                                        }
+                                        </strong>
+                                    <button className="bg-zinc-900 p-1 rounded">
+                                        <BsCartPlus size={20} color="#fff" />
+                                    </button>
+                                </div>
+                            </section>
+                        )
+                    })
+                }
+
+            </div>
         </main>
     )
 }
