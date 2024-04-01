3f9ff2447b979467b6c4c704b51371059999926a
dev-herbert <soyzabeto@gmail.com>
2024-03-31 23:18:26 -0300

    feat: Adicionado carrinho de estrutura do produto e do produto dentro do carrinho

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -3,9 +3,40 @@
 
 export const Cart = () => {
 
-    return(
-        <>
-        <h1 className="font-bold text-7xl">Carrinho</h1>
-        </>
+    return (
+        <main className="w-full max-w-7xl mx-auto">
+
+            <h1 className="font-medium text-2xl text-center my-4">Meu Carrinho</h1>
+
+            <section className="flex items-center justify-between border-b-2 border-gray-200">
+
+                <img
+                    className="w-28"
+                    src="https://m.media-amazon.com/images/I/61CVih3UpdL.__AC_SY445_SX342_QL70_ML2_.jpg"
+                    alt="logo de um fone de ouvido"
+                />
+
+                <strong>
+                    R$ 1000,00
+                </strong>
+
+                <div className="flex items-center justify-center gap-3">
+                    <button className="bg-slate-600 px-2 text-white rounded font-medium flex items-center justify-center">
+                        -
+                    </button>
+                    2
+                    <button className="bg-slate-600 px-2 text-white rounded font-medium flex items-center justify-center">
+                        +
+                    </button>
+                </div>
+
+                <strong className="float-right">
+                    SubTotal: R$ 1000,00
+                </strong>
+
+            </section>
+
+            <p className="font-bold mt-4">Total R$ 1002,00</p>
+        </main>
     )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,35 @@
-
+import { BsCartPlus } from "react-icons/bs"
 
 
 
 export const Home = () => {
 
-    return (
+    return(
         <main className="w-full max-w-7xl mx-auto px-4">
-            <h1 className="font-bold mt-10 mb-4 text-center text-2xl ">
-                Produtos em alta
-            </h1>
-            {/* lista de produtos */}
-            <div>
-                <section>
-                    <img
-                        src="https://m.media-amazon.com/images/I/61CVih3UpdL.__AC_SY445_SX342_QL70_ML2_.jpg"
-                        alt="logo de um fone de ouvido"
-                    />
+        <h1 className="font-bold mt-10 mb-4 text-center text-2xl ">
+            Produtos em alta
+        </h1>
+        {/* lista de produtos */}
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5 ">
+            <section>
+                <img 
+                className="w-full rounded-lg max-h-70 mb-2 "
+                src="https://m.media-amazon.com/images/I/61CVih3UpdL.__AC_SY445_SX342_QL70_ML2_.jpg" 
+                alt="logo de um fone de ouvido" 
+                />
 
-                    <p>
-                        apple AirPode (3a geração )
-                    </p>
-                </section>
-            </div>
+                <p className="font-medium mt-1 mb-2">
+                    apple AirPode (3a geração )
+                </p>
+
+                <div className="flex gap-3 items-center">
+                    <strong className="text-zinc-700/70">R$ 1.200</strong>
+                    <button className="bg-zinc-900 p-1 rounded">
+                        <BsCartPlus size={20} color="#fff"/>
+                    </button>
+                </div>
+            </section>
+        </div>
         </main>
     )
 }
